d2167def45ca551e7d6bb49582ea356b3a144c0b
Brendan <2bndy5@gmail.com>
2024-02-12 01:34:54 -0800

    automatically set docs' copyright and version

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,8 @@
 import sys
 import re
 from pathlib import Path
+import time
+from importlib.metadata import version as get_version
 from sphinx.application import Sphinx
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
@@ -15,9 +17,9 @@ from cpp_linter.cli import cli_arg_parser  # noqa: E402
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 project = "cpp-linter"
-copyright = "2022, 2bndy5"
+copyright = f"{time.localtime().tm_year}, 2bndy5"
 author = "2bndy5"
-release = "2.0.0"
+release = get_version("cpp-linter")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
