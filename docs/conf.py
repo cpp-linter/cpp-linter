452d52eaf2ddba5a0220b9c57ee1a29fd1c9e624
Brendan <2bndy5@gmail.com>
2024-02-29 22:18:19 -0800

    Revise CLI doc generation
    adds permission links and required version for each input option

--- docs/conf.py
+++ docs/conf.py
@@ -3,11 +3,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import re
 from pathlib import Path
 import time
+from typing import Optional
 from importlib.metadata import version as get_version
+import docutils
 from sphinx.application import Sphinx
+from sphinx.util.docutils import SphinxRole
+from sphinx_immaterial.inline_icons import load_svg_into_builder_env
 from cpp_linter.cli import cli_arg_parser
 
 # -- Project information -----------------------------------------------------
@@ -57,7 +60,7 @@ html_theme_options = {
             "media": "(prefers-color-scheme: light)",
             "scheme": "default",
             "primary": "light-blue",
-            "accent": "deep-purple",
+            "accent": "cyan",
             "toggle": {
                 "icon": "material/lightbulb-outline",
                 "name": "Switch to dark mode",
@@ -67,7 +70,7 @@ html_theme_options = {
             "media": "(prefers-color-scheme: dark)",
             "scheme": "slate",
             "primary": "light-blue",
-            "accent": "deep-purple",
+            "accent": "cyan",
             "toggle": {
                 "icon": "material/lightbulb",
                 "name": "Switch to light mode",
@@ -109,34 +112,138 @@ for name in ("hint", "tip", "important"):
 # -- Parse CLI args from `-h` output -------------------------------------
 
 
+class CliBadge(SphinxRole):
+    badge_type: str
+    badge_icon: Optional[str] = None
+    href: Optional[str] = None
+    href_title: Optional[str] = None
+
+    def run(self):
+        permission_link = ""
+        if self.badge_type == "permission":
+            permission_link, permission = self.text.split(" ", 1)
+            self.text = permission
+        is_linked = ""
+        if self.href is not None and self.href_title is not None:
+            is_linked = (
+                f'<a href="{self.href}{permission_link or self.text}" '
+                + f'title="{self.href_title}">'
+            )
+        head = '<span class="mdx-badge__icon">'
+        if not self.badge_icon:
+            head += self.badge_type.title()
+        else:
+            head += is_linked
+            head += (
+                f'<span class="md-icon si-icon-inline {self.badge_icon}"></span></a>'
+            )
+        head += "</span>"
+        header = docutils.nodes.raw(
+            self.rawtext,
+            f'<span class="mdx-badge">{head}<span class="mdx-badge__text">'
+            + is_linked
+            + (self.text if self.badge_type == "version" else ""),
+            format="html",
+        )
+        if self.badge_type != "version":
+            old_highlight = self.inliner.document.settings.syntax_highlight
+            self.inliner.document.settings.syntax_highlight = "yaml"
+            code, sys_msgs = docutils.parsers.rst.roles.code_role(
+                role="code",
+                rawtext=self.rawtext,
+                text=self.text,
+                lineno=self.lineno,
+                inliner=self.inliner,
+                options={"language": "yaml", "classes": ["highlight"]},
+                content=self.content,
+            )
+            self.inliner.document.settings.syntax_highlight = old_highlight
+        else:
+            code, sys_msgs = ([], [])
+        tail = "</span></span>"
+        if self.href is not None and self.href_title is not None:
+            tail = "</a>" + tail
+        trailer = docutils.nodes.raw(self.rawtext, tail, format="html")
+        return ([header, *code, trailer], sys_msgs)
+
+
+class CliBadgeVersion(CliBadge):
+    badge_type = "version"
+    href = "https://github.com/cpp-linter/cpp-linter/releases/v"
+    href_title = "Required Version"
+
+    def run(self):
+        self.badge_icon = load_svg_into_builder_env(
+            self.env.app.builder, "material/tag-outline"
+        )
+        return super().run()
+
+
+class CliBadgeDefault(CliBadge):
+    badge_type = "Default"
+
+
+class CliBadgePermission(CliBadge):
+    badge_type = "permission"
+    href = "permissions.html#"
+    href_title = "Required Permission"
+
+    def run(self):
+        self.badge_icon = load_svg_into_builder_env(
+            self.env.app.builder, "material/lock"
+        )
+        return super().run()
+
+
+REQUIRED_VERSIONS = {
+    "1.7.0": ["tidy_review", "format_review"],
+    "1.6.1": ["thread_comments", "no_lgtm"],
+    "1.6.0": ["step_summary"],
+    "1.4.7": ["extra_arg"],
+}
+
+PERMISSIONS = {
+    "thread_comments": ["thread-comments", "issues: write"],
+    "tidy_review": ["pull-request-reviews", "pull_request: write"],
+    "format_review": ["pull-request-reviews", "pull_request: write"],
+    "files_changed_only": ["file-changes", "contents: read"],
+    "lines_changed_only": ["file-changes", "contents: read"],
+}
+
+
 def setup(app: Sphinx):
     """Generate a doc from the executable script's ``--help`` output."""
+    app.add_role("badge-version", CliBadgeVersion())
+    app.add_role("badge-default", CliBadgeDefault())
+    app.add_role("badge-permission", CliBadgePermission())
 
-    output = cli_arg_parser.format_help()
-    first_line = re.search(r"^options:\s*\n", output, re.MULTILINE)
-    if first_line is None:
-        raise OSError("unrecognized output from `cpp-linter -h`")
-    output = output[first_line.end(0) :]
+    args = cli_arg_parser._optionals._actions
     doc = "Command Line Interface Options\n==============================\n\n"
     doc += ".. note::\n\n    These options have a direct relationship with the\n    "
     doc += "`cpp-linter-action user inputs "
-    doc += "<https://github.com/cpp-linter/cpp-linter-action#optional-inputs>`_. "
+    doc += "<https://cpp-linter.github.io/cpp-linter-action/inputs-outputs#inputs>`_. "
     doc += "Although, some default values may differ.\n\n"
-    CLI_OPT_NAME = re.compile(
-        r"^\s*(\-[A-Za-z]+)\s?\{?[A-Za-z_,0-9]*\}?,\s(\-\-[^\s]*?)\s"
-    )
-    for line in output.splitlines():
-        match = CLI_OPT_NAME.search(line)
-        if match is not None:
-            # print(match.groups())
-            doc += "\n.. std:option:: " + ", ".join(match.groups()) + "\n\n"
-            options_match = re.search(
-                r"\-\w\s\{[a-zA-Z,0-9]+\},\s\-\-[\w\-]+\s\{[a-zA-Z,0-9]+\}", line
-            )
-            if options_match is not None:
-                new_txt = options_match.group()
-                line = line.replace(options_match.group(), f"``{new_txt}``")
-        doc += line + "\n"
+    for arg in args:
+        aliases = arg.option_strings
+        if not aliases or arg.default == "==SUPPRESS==":
+            continue
+        doc += "\n.. std:option:: " + ", ".join(aliases) + "\n"
+        help = arg.help[: arg.help.find("Defaults to ")]
+        for ver, names in REQUIRED_VERSIONS.items():
+            if arg.dest in names:
+                req_ver = ver
+                break
+        else:
+            req_ver = "1.4.6"
+        doc += f"\n    :badge-version:`{req_ver}` "
+        doc += f":badge-default:`'{arg.default or ''}'` "
+        for name, permission in PERMISSIONS.items():
+            if name == arg.dest:
+                link, spec = permission
+                doc += f":badge-permission:`{link} {spec}`"
+                break
+        doc += "\n\n    "
+        doc += "\n    ".join(help.splitlines()) + "\n"
     cli_doc = Path(app.srcdir, "cli_args.rst")
     cli_doc.unlink(missing_ok=True)
     cli_doc.write_text(doc)
