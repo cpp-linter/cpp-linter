59d384957f0b5922951f2388bc22f02e243d76cd
Matthew Oliver <Matthew.Oliver@amd.com>
2024-12-06 21:01:20 +1100

    Add graphql mock responses.

--- tests/reviews/test_pr_review.py
+++ tests/reviews/test_pr_review.py
@@ -139,6 +139,66 @@ def test_post_review(
         for review_id in [r["id"] for r in json.loads(reviews) if "id" in r]:
             mock.put(f"{base_url}/reviews/{review_id}/dismissals")
         extensions = ["cpp", "hpp", "c"]
+
+        # mock graphql requests
+        graphql_url = f"{gh_client.api_url}/graphql"
+
+        def graphql_callback(request, context):
+            context.status_code = 200
+            if request.data.find("query") is not -1:
+                # get existing review comments
+                return (cache_path / "pr_reviews_graphql.json").read_text(
+                    encoding="utf-8"
+                )
+            elif request.data.find("resolveReviewThread") is not -1:
+                # resolve review
+                id_pos = request.data.find('threadId:"') + 10
+                id_end_pos = request.data.find('"', id_pos + 1)
+                id = request.data[id_pos, id_end_pos]
+                return (
+                    """{
+  "data": {
+    resolveReviewThread: {
+      thread {
+        %s
+      }
+    }
+  }
+}"""
+                    % id
+                )
+            elif request.data.find("deletePullRequestReviewComment") is not -1:
+                # delete PR or minimizeComment
+                id_pos = request.data.find('id:"') + 4
+                id_end_pos = request.data.find('"', id_pos + 1)
+                id = request.data[id_pos, id_end_pos]
+                return (
+                    """{
+  "data": {
+    deletePullRequestReviewComment: {
+      pullRequestReviewComment {
+        %s
+      }
+    }
+  }
+}"""
+                    % id
+                )
+            elif request.data.find("minimizeComment") is not -1:
+                # minimizeComment
+                return """{
+  "data": {
+    "minimizeComment": {
+      "minimizedComment": {
+        "isMinimized": true
+      }
+    }
+  }
+}"""
+            return "errors"
+
+        mock.post(graphql_url, text=graphql_callback)
+
         # run the actual test
         files = gh_client.get_list_of_changed_files(
             FileFilter(extensions=extensions),
