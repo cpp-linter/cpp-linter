39c646edb33087ba475468f9d0517efae8091537
Matthew Oliver <Matthew.Oliver@amd.com>
2024-12-18 15:54:50 +1100

    Apply suggestions from code review
    Co-authored-by: Brendan <2bndy5@gmail.com>

--- cpp_linter/rest_api/github_api.py
+++ cpp_linter/rest_api/github_api.py
@@ -570,8 +570,13 @@ class GithubApiClient(RestApiClient):
                 file_obj, summary_only, review_comments
             )
 
-    def _dismiss_stale_reviews(self, url: str, ignored_reviews: List[str] = None):
-        """Dismiss all reviews that were previously created by cpp-linter"""
+    def _dismiss_stale_reviews(self, url: str, ignored_reviews: Optional[List[str]] = None):
+        """Dismiss reviews that were previously created by cpp-linter.
+        
+        Use ``ignored_reviews`` to only dismiss the reviews not specified in
+        the given list. If ``ignored_reviews`` is `None`, then dismiss all
+        reviews.
+        """
         next_page: Optional[str] = url + "?page=1&per_page=100"
         while next_page:
             response = self.api_request(url=next_page)
@@ -585,8 +590,11 @@ class GithubApiClient(RestApiClient):
                     and "state" in review
                     and review["state"] not in ["PENDING", "DISMISSED"]
                     and (
-                        ignored_reviews is not None
-                        and review["node_id"] not in ignored_reviews
+                        ignored_reviews is None
+                        or (
+                            "node_id" in review
+                            and review["node_id"] not in ignored_reviews
+                        )
                     )
                 ):
                     assert "id" in review
